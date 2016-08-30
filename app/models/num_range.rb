31647c567197f998d1a9689f761abc6a75889511
BranLiang <lby89757@hotmail.com>
2016-08-30 19:15:55 +0800

    modify the existence

--- app/models/num_range.rb
+++ app/models/num_range.rb
@@ -4,4 +4,8 @@ class NumRange < ApplicationRecord
   validates_numericality_of :maximum, greater_than: :minimum
   validates_numericality_of :minimum, less_than: :maximum
   validates :text, :required, presence: true
+
+  def require_status
+    self.required ? "Required" : "Not required"
+  end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -8,7 +8,7 @@ class Survey < ApplicationRecord
                           length: { within: 1..1000 }
 
 
-  def has_no_questions_with_text?
+  def has_no_multi_questions_with_text?
     all_questions = self.questions
     if all_questions.empty?
       return true
@@ -19,7 +19,28 @@ class Survey < ApplicationRecord
     end
   end
 
-  def all_questions_with_text
-    self.questions.select(&:has_text?)
+  def has_multi_questions?
+    !has_no_multi_questions_with_text?
+  end
+
+  def has_num_ranges?
+    !has_no_num_ranges?
+  end
+
+  def has_no_num_ranges?
+    self.num_ranges.empty? ? true : false
+  end
+
+  def has_no_questions?
+    has_no_multi_questions_with_text? &&
+    has_no_num_ranges?
+  end
+
+  def all_multi_questions_with_text
+    self.questions.where.not(text: nil)
+  end
+
+  def all_num_questions
+    self.num_ranges
   end
 end
